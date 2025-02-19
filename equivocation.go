aa9a9566992097cabbb0460d94cf6461d062f489
parthshah1 <parthhshah171@gmail.com>
2025-02-19 18:42:00 +0530

    Patching equivocation filter

--- equivocation.go
+++ equivocation.go
@@ -1,7 +1,6 @@
 package f3
 
 import (
-	"bytes"
 	"slices"
 	"sync"
 
@@ -79,39 +78,13 @@ func (ef *equivocationFilter) ProcessBroadcast(m *gpbft.GMessage) bool {
 		ef.activeSenders = make(map[gpbft.ActorID]equivSenders)
 	}
 
-	key := ef.formKey(m)
-	msgInfo, ok := ef.seenMessages[key]
-	equivocationDetected := false
-	if ok && !bytes.Equal(msgInfo.signature, m.Signature) {
-		if msgInfo.origin == ef.localPID {
-			log.Warnw("local self-equivocation detected", "sender", m.Sender,
-				"instance", m.Vote.Instance, "round", m.Vote.Round, "phase", m.Vote.Phase)
-			return false
-		} else {
-			log.Warnw("detected equivocation during broadcast", "sender", m.Sender,
-				"instance", m.Vote.Instance, "round", m.Vote.Round, "phase", m.Vote.Phase)
-			equivocationDetected = true
-		}
-	} else if !ok {
-		// save the signature
-		ef.seenMessages[key] = equivMessage{signature: m.Signature, origin: ef.localPID}
-	}
 	// save ourselves as one of the senders
 	senders := ef.activeSenders[m.Sender]
-	senders.addSender(ef.localPID, equivocationDetected)
+	senders.addSender(ef.localPID, false)
 	ef.activeSenders[m.Sender] = senders
 
-	if !senders.equivocation {
-		// we are alone in this dark forest
-		return true
-	}
-	// if we are not alone, broadcast the message if we have the best (lowest PeerID)
-
-	log.Warnw("self-equivocation detected during broadcast", "sender", m.Sender, "instance", m.Vote.Instance,
-		"round", m.Vote.Round, "phase", m.Vote.Phase, "sourcers", senders, "localPID", ef.localPID)
-
-	// if there are multiple senders, only broadcast if we are the smallest one
-	return senders.origins[0] == ef.localPID
+	// always allow broadcast
+	return true
 }
 
 func (ef *equivocationFilter) ProcessReceive(peerID peer.ID, m *gpbft.GMessage) {
@@ -122,19 +95,14 @@ func (ef *equivocationFilter) ProcessReceive(peerID peer.ID, m *gpbft.GMessage) 
 		// the instance does not match
 		return
 	}
-	senders, ok := ef.activeSenders[m.Sender]
+	_, ok := ef.activeSenders[m.Sender]
 	if !ok {
 		// we do not track the sender because we didn't send any messages from that ID
 		// otherwise we would have to track all messages
 		return
 	}
 	key := ef.formKey(m)
-	msgInfo, ok := ef.seenMessages[key]
-	if ok && !bytes.Equal(msgInfo.signature, m.Signature) {
-		// equivocation detected
-		senders.addSender(peerID, true)
-		ef.activeSenders[m.Sender] = senders
-	}
+	_, ok = ef.seenMessages[key]
 	if !ok {
 		// add the message
 		ef.seenMessages[key] = equivMessage{signature: m.Signature, origin: peerID}
